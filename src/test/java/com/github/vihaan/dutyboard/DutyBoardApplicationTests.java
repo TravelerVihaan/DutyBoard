83e2bac5fc5f707dad4313eca1b28e7970784888
TravelerVihaan <lukasz.biel@10g.pl>
2020-08-06 11:30:51 +0200

    Initial commit

---
+++ src/test/java/com/github/vihaan/dutyboard/DutyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vihaan.dutyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutyBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
