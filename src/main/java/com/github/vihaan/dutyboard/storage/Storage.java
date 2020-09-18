1a2e08b58fce571df174053495767a2658f767e8
TravelerVihaan <lukasz.biel@10g.pl>
2020-09-19 01:50:07 +0200

    Storage interface

--- src/main/java/com/github/vihaan/dutyboard/storage/Storage.java
+++ src/main/java/com/github/vihaan/dutyboard/storage/Storage.java
@@ -1,6 +1,12 @@
 package com.github.vihaan.dutyboard.storage;
 
+import com.github.vihaan.dutyboard.worker.Worker;
+
+import java.util.Collection;
+
 public interface Storage {
 
+    Worker getWorkerByNameOrSurname(String name);
 
+    Collection<Worker> getAllWorkers();
 }
