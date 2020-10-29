7f6b9f012162823bc1cc77b47556084b88566db1
TravelerVihaan <lukasz.biel@10g.pl>
2020-10-29 23:51:22 +0100

    BoardsService, createStorage

--- src/main/java/com/github/vihaan/dutyboard/dutyboard/BoardsService.java
+++ src/main/java/com/github/vihaan/dutyboard/dutyboard/BoardsService.java
@@ -1,5 +1,7 @@
 package com.github.vihaan.dutyboard.dutyboard;
 
+import com.github.vihaan.dutyboard.storage.Storage;
+import com.github.vihaan.dutyboard.storage.StorageType;
 import com.github.vihaan.dutyboard.storage.factory.StorageFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,11 +10,14 @@ import org.springframework.stereotype.Service;
 public class BoardsService {
 
     private StorageFactory storageFactory;
+    private Storage storage;
 
     @Autowired
     public BoardsService(StorageFactory storageFactory){
         this.storageFactory = storageFactory;
     }
 
-
+    public void getSpecificStorage(StorageType storageType){
+        storage = storageFactory.createStorage(storageType);
+    }
 }
