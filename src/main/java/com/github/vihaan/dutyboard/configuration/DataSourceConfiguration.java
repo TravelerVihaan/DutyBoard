033cca52be403ce12420a8eab3ca873b0bef16ff
TravelerVihaan <lukasz.biel@10g.pl>
2020-10-30 02:12:46 +0100

    Datasources

---
+++ src/main/java/com/github/vihaan/dutyboard/configuration/DataSourceConfiguration.java
@@ -0,0 +1,8 @@
+package com.github.vihaan.dutyboard.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfiguration {
+    // TODO Configure primary and secondary datasource if it will be necessary
+}
