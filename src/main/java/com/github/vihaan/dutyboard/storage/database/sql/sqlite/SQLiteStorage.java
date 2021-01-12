9ac3e04acef5615c5e89041f20744dcf848f594b
TravelerVihaan <lukasz.biel@10g.pl>
2021-01-12 22:32:09 +0100

    Update SQLiteStorage and Spring Boot version

--- src/main/java/com/github/vihaan/dutyboard/storage/database/sql/sqlite/SQLiteStorage.java
+++ src/main/java/com/github/vihaan/dutyboard/storage/database/sql/sqlite/SQLiteStorage.java
@@ -1,11 +1,15 @@
 package com.github.vihaan.dutyboard.storage.database.sql.sqlite;
 
+import com.github.vihaan.dutyboard.duty.DutyType;
+import com.github.vihaan.dutyboard.dutyboard.Board;
 import com.github.vihaan.dutyboard.storage.database.sql.SQLStorage;
 import com.github.vihaan.dutyboard.worker.Worker;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 @Qualifier("sqlite-storage")
@@ -18,6 +22,36 @@ public class SQLiteStorage extends SQLStorage{
     @Override
     public Collection<Worker> getAllWorkers() {
         return null;
+    }
+
+    @Override
+    public Set<Board> getBoards() {
+        return null;
+    }
+
+    @Override
+    public Optional<Board> getBoard(DutyType dutyType) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean saveBoard(Board board) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteBoard(Board board) {
+        return false;
+    }
+
+    @Override
+    public boolean saveWorker(Worker worker) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteWorker(Worker worker) {
+        return false;
     }
 }
 
