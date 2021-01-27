a98d43e5e9ae617828092908265704304a9c76a3
TravelerVihaan <lukasz.biel@10g.pl>
2021-01-27 23:15:04 +0100

    Add validators and cleanup

--- src/main/java/com/github/vihaan/dutyboard/storage/StorageType.java
+++ src/main/java/com/github/vihaan/dutyboard/storage/StorageType.java
@@ -2,9 +2,9 @@ package com.github.vihaan.dutyboard.storage;
 
 public enum StorageType {
     TEXT(StorageConstants.TEXT_FILE_STORAGE),
-    JSON(StorageConstants.JSON_FILE_STORAGE),
-    SQLITE(StorageConstants.SQLITE_STORAGE),
-    MONGO(StorageConstants.MONGO_STORAGE);
+    JSON(StorageConstants.JSON_FILE_STORAGE);
+//    SQLITE(StorageConstants.SQLITE_STORAGE),
+//    MONGO(StorageConstants.MONGO_STORAGE);
 
     String storageType;
 
---
+++ src/main/java/com/github/vihaan/dutyboard/validators/WorkerValidator.java
@@ -0,0 +1,4 @@
+package com.github.vihaan.dutyboard.validators;
+
+public class WorkerValidator {
+}
